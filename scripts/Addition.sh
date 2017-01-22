4cbdcebcc273fbbbde3c055a874b960de6c21def
auspbro <ryan.xue@quantacn.com>
2017-01-22 14:13:00 +0800

    Mark invalid command

--- scripts/Addition.sh
+++ scripts/Addition.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo .Enter the First Number: .
+
+#echo .Enter the First Number: .
 read -p "Enter the First Number:" a
-echo .Enter the Second Number: .
-read -p "Enter the First Number:" b
+#echo .Enter the Second Number: .
+read -p "Enter the Second Number:" b
 x=$(expr "$a" + "$b")
 echo $a + $b = $x
 
