17a19f2d91f9cd5ad7fe516e82f1a7f1b6d7fd1b
auspbro <ryan.xue@quantacn.com>
2017-01-22 15:01:11 +0800

    add condition selected

--- scripts/Addition.sh
+++ scripts/Addition.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-#echo .Enter the First Number: .
-read -p "Enter the First Number:" a
-#echo .Enter the Second Number: .
-read -p "Enter the Second Number:" b
+# 使用read [-t] 参数计时输入
+if read -t 10 -p "Enter the First Number:" a
+then
+	echo "hello, welcome to my script."
+else
+	echo "Sorry, too slow."
+fi
+
+
+if read -t 10 -p "Enter the Second Number:" b
+then
+	echo "Good job!"
+else
+	echo "Sorry, too slow."
+
 x=$(expr "$a" + "$b")
 echo $a + $b = $x
 
