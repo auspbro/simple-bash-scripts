bd8861dbb37fe6dd2d5faafb9c9e722255e8ba5c
auspbro <auspbro@gmail.com>
2017-08-31 15:43:23 +0800

    add input time count backwards

--- scripts/Addition.sh
+++ scripts/Addition.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+<<<<<<< HEAD
 
 # 使用read [-t] 参数计时输入
-if read -t 10 -p "Enter the First Number:" a
+if read -t 10 -p "Enter the First Number in 10 Seconds:" a
 then
 	echo "hello, welcome to my script."
 else
@@ -9,13 +10,19 @@ else
 fi
 
 
-if read -t 10 -p "Enter the Second Number:" b
+if read -t 10 -p "Enter the Second Number in 10 Seconds:" b
 then
 	echo "Good job!"
 else
 	echo "Sorry, too slow."
 fi
 
+=======
+echo .Enter the First Number: .
+read a
+echo .Enter the Second Number: .
+read b
+>>>>>>> 6fff862b1c9ff40a081e1cd6a687fb23dab3b50c
 x=$(expr "$a" + "$b")
 echo $a + $b = $x
 
