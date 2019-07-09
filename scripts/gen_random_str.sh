214b37ccbfa95b4f33e6aefe502f53e3f81bf4b2
Ryan <auspbro@gmail.com>
2019-07-09 09:32:36 -0400

    Add execute permission

--- scripts/gen_random_str.sh
+++ scripts/gen_random_str.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #使用for循环在/oldboy目录下批量创建10个html文件，其中每个文件需要包含10个随机小写字母加固定字符串oldboy
 
-[[ -d ./oldboy ]] || mkdir ./oldboy
-cd ./oldboy 
+[[ -d /tmp/oldboy ]] || mkdir -p /tmp/oldboy
+cd /tmp/oldboy 
 
 for i in `seq 10` ; do
     filename=`echo $RANDOM | md5sum | tr '0-9' 'a-z' | cut -c 1-10`
